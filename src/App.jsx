67ff4c77a94eea083a646931a99b24f8a3c75a96
rakaso598 <nexonaddress98@gmail.com>
2025-02-28 10:27:07 +0900

    feat: 상품리스트 마운트 정렬

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { ProductApi } from "./components/ProductApi.jsx"
+import { FavoriteProductList, RecentProductList } from "./components/ProductApi.jsx"
+import "./App.css";
 
 function App() {
 
   return (
     <>
-      <ProductApi orderBy='favorite' />
+      <div className="favoriteProductList">
+        <h2>베스트 순 정렬</h2>
+        <FavoriteProductList />
+      </div>
+      <div className="recentProductList">
+        <h2>최근 순 정렬</h2>
+        <RecentProductList />
+      </div>
+
     </>
   )
 }
--- src/components/ProductApi.jsx
+++ src/components/ProductApi.jsx
@@ -1,21 +1,53 @@
 import { useState, useEffect } from "react";
 import { getProductList } from "../services/ProductService.js";
 
-export const ProductApi = (orderBy) => {
+export const RecentProductList = () => {
   const [productList, setProductList] = useState(null);
 
-  useEffect((orderBy) => {
+  useEffect(() => {
     const fetchProductList = async () => {
       try {
-        const productListResponse = await getProductList(1, 10, orderBy, undefined);
+        const productListResponse = await getProductList(1, 10, 'recent');
         setProductList(productListResponse);
       } catch (err) {
         console.error(err);
       }
     };
-
     fetchProductList();
-  }, [orderBy]);
+  }, []);
+
+  if (!productList || !productList.list) {
+    return <div>No products found.</div>;
+  }
+
+  return (
+    <>
+      {productList.list.map((product) => {
+        return (
+          <div key={product.id}>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export const FavoriteProductList = () => {
+  const [productList, setProductList] = useState(null);
+
+  useEffect(() => {
+    const fetchProductList = async () => {
+      try {
+        const productListResponse = await getProductList(1, 10, 'favorite');
+        setProductList(productListResponse);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchProductList();
+  }, []);
 
   if (!productList || !productList.list) {
     return <div>No products found.</div>;
